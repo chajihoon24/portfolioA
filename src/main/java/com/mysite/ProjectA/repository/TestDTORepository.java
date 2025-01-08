692633acd60755f47a75de101a336659d6d19a84
DESKTOP-DAIRV42\차지훈 <jihoon9998@naver.com>
2025-01-08 09:55:13 +0900

    메인페이지 사진추가

--- src/main/java/com/mysite/ProjectA/repository/TestDTORepository.java
+++ src/main/java/com/mysite/ProjectA/repository/TestDTORepository.java
@@ -3,7 +3,6 @@ package com.mysite.ProjectA.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mysite.ProjectA.DAO.TestDAO;
-import com.mysite.ProjectA.DTO.TestDTO;
 
 public interface TestDTORepository extends JpaRepository<TestDAO, Long>{
 	
